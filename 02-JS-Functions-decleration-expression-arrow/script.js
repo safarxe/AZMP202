5366ebc7a5b37ac200e7208f1c962b8fe6fc9a4a
safarxe@gmail.com <safarxe@gmail.com>
2024-11-05 11:43:01 +0400

    commit

--- 02-JS-Functions-decleration-expression-arrow/script.js
+++ 02-JS-Functions-decleration-expression-arrow/script.js
@@ -6,3 +6,18 @@ function deleteRepeate(arr) {
 console.log(deleteRepeate([1, 2, 2, 3, 4, 4, 5]))
 console.log(deleteRepeate([1, 2, 3, 4, 5]))
 console.log(deleteRepeate([1, -2, -2, 3, 4, -5, -6, -5]))
+
+//! Daxil edilən cümlədə necə söz olduğun tapan funksiya yazın.
+
+function countWords(sentence) {
+    let count = 0;
+    for (let i = 0; i < sentence.length; i++) {
+        if (sentence[i] !== " ") {
+            count++;
+        }
+    }
+
+    return count;
+}
+console.log(countWords("hello  my friend"));
+
