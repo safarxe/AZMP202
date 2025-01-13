46036967f3501aac319729ef66ca25c2e7422b58
codexi <noureyvzz@gmail.colm>
2025-01-13 19:26:02 +0400

    commit

---
+++ classwork-wines/server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const productRouter = require("./routes/productRoute");
+
+const DB_URL =
+  "mongodb+srv://safar:safar@cluster0.xsi5e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const PASSWORD = "safar";
+const PORT = 8080;
+const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+app.use(express.json());
+app.use("/api/products", productRouter);
+
+mongoose.connect(DB_URL).then(() => {
+  console.log("Connected!");
+  app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+  });
+});
